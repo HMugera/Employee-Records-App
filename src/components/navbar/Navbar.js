68f8bebd25f7da0386b338382a9286763b5a8256
Hmugera <mugerahughes@gmail.com>
2020-09-30 08:46:04 +0300

    updating styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,8 +7,21 @@ import { Button } from "../button/Button";
 
 export default function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton =()=>{
+    if(window.innerWidth <= 960){
+      setButton(false);
+    }else{
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize',showButton);
+   
   return (
     <>
       <div className="navbar">
